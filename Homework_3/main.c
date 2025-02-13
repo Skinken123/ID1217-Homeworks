3f1ee89ffa27a531fb91041994b387eb16caf2b0
Gustav Moritz <gmoritz@kth.se>
2025-02-13 12:06:32 +0100

    ching chong

--- Homework_3/main.c
+++ Homework_3/main.c
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
+#define SHARED 1
+#define MAX_WORKERS 16
+
+sem_t enterBathroom, womenWait, menWait;
+int nrOfWaitingMen, nrOfWaitingWomen, nrOfPeopleInBathroom;
 
 void *man(void *arg) {
 
@@ -15,5 +20,11 @@ void *women(void *arg) {
 }
 
 int main(int argc, char *argv[]) {
-
+    pthread_t workerId[MAX_WORKERS];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+    sem_init(&enterBathroom, SHARED, 1);
+    sem_init(&womenWait, SHARED, 0);
+    sem_init(&menWait, SHARED, 0);
 }
