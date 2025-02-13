3956c8694ea701c22857bb896d7d05b556f0406e
Gustav Moritz <gmoritz@kth.se>
2025-02-13 11:34:54 +0100

    hw3 main file

---
+++ Homework_3/main.c
@@ -0,0 +1,19 @@
+#ifndef _REENTRANT
+#define _REENTRANT
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+void *man(void *arg) {
+
+}
+
+void *women(void *arg) {
+    
+}
+
+int main(int argc, char *argv[]) {
+
+}
