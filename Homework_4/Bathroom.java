bdd7140520ec9b63b1fc35d0ea9bcffe8a3610f1
Gustav Moritz <gmoritz@kth.se>
2025-02-18 17:09:32 +0100

    change

--- Homework_4/Bathroom.java
+++ Homework_4/Bathroom.java
@@ -1,5 +1,4 @@
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 public class Bathroom {
     //Block synchronization
--- Homework_4/MainProgram.java
+++ Homework_4/MainProgram.java
@@ -1,7 +1,7 @@
 public class MainProgram {
     public static void main(String[] args) {
         final int MAX_WORKERS = 16;
-        Bathroom bathroom = new Bathroom();
+        final Bathroom bathroom = new Bathroom();
 
         for (int i = 0; i < MAX_WORKERS / 2; i++) {
             new Thread(new Person(bathroom, 'M', i)).start();
