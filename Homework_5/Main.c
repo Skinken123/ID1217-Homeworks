f2a6e095f7bb976ef104014d0a2da3030cbf201a
Alexander Nordgren <151035630+alnordg@users.noreply.github.com>
2025-02-21 10:57:42 +0100

    setup done

---
+++ Homework_5/Main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "algorithms.h"
+#include "benchmark.h"
+
+int main(int argc, char **argv) {
+    
+}
---
+++ Homework_5/algorithms.h
@@ -0,0 +1,12 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void centralized_solution();
+void symmetric_solution();
+void ring_solution();
+
+#endif /* ALGORITHMS_H */
